6310292be116e47fc0275259271c640e445b7ab6
Jongwon Kim <63104990+jwon-kim@users.noreply.github.com>
2020-07-31 14:20:05 +0900

    Update time.py

--- time.py
+++ time.py
@@ -3,7 +3,7 @@ import time
 def time_wrapper(func):
     def decorator(*args, **kwargs):
         start_time = time.time()
-        print("%s %s" % (func.__name__, " is executed"))
+        print("%s %s" % (func.__name__, " is started"))
         result = func(*args, **kwargs)
         print("%s %s %s %s" % (func.__name__ , " is finished. It takes ", str(time.time()-start_time), "seconds"))
         return result
